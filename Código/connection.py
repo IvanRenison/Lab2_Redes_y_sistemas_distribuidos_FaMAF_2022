5e9deb1b2f2899bf33317fa185ca30a8303740ba
alvaro-sch <alvarolegit@gmail.com>
2022-04-03 16:15:12 -0300

    Formateado a favor de pep8, removido código irrelevante

--- Código/connection.py
+++ Código/connection.py
@@ -13,6 +13,7 @@ import time
 import sys
 import traceback
 
+
 class Connection(object):
     """
     Conexión punto a punto entre el servidor y un cliente.
@@ -20,7 +21,7 @@ class Connection(object):
     que termina la conexión.
     """
 
-    def __init__(self, socket : socket.socket, directory : str):
+    def __init__(self, socket: socket.socket, directory: str):
         # Inicialización de conexión
         self.socket = socket
         self.directory = directory
@@ -61,7 +62,7 @@ class Connection(object):
         self.connection_active = False
         print("Closing connection...")
 
-    def __mk_code(self, code : int) -> str:
+    def __mk_code(self, code: int) -> str:
         assert code in error_messages.keys()
 
         return f"{code} {error_messages[code]}"
@@ -69,12 +70,12 @@ class Connection(object):
     def file_exist(self, filename: str) -> bool:
         return os.path.isfile(os.path.join(self.directory, filename))
 
-    def filename_is_valid(self, filename : str) -> bool:
+    def filename_is_valid(self, filename: str) -> bool:
 
         invalid_chars = set(filename) - VALID_CHARS
         return (len(invalid_chars) == 0)
 
-    def analizar_comando(self, command : list):
+    def analizar_comando(self, command: list):
         """
         Analiza el comando y ejecuta la función correspondiente
         """
@@ -91,7 +92,6 @@ class Connection(object):
                 self.buffer_out = self.__mk_code(INVALID_ARGUMENTS)
                 self.send(self.buffer_out)
 
-
         elif command_name == 'get_metadata':
             if len(args_name) == 2:
                 self.get_metadata(args_name[1])
@@ -99,14 +99,12 @@ class Connection(object):
                 self.buffer_out = self.__mk_code(INVALID_ARGUMENTS)
                 self.send(self.buffer_out)
 
-
         elif command_name == 'get_slice':
             if len(args_name) == 4:
                 self.get_slice(args_name[1], args_name[2], args_name[3])
             else:
                 self.buffer_out = self.__mk_code(INVALID_ARGUMENTS)
                 self.send(self.buffer_out)
-
 
         elif command_name == 'quit':
             if len(args_name) == 1:
@@ -116,10 +114,9 @@ class Connection(object):
                 self.send(self.buffer_out)
 
         else:
-            #comando desconocido
+            # comando desconocido
             self.buffer_out = self.__mk_code(INVALID_COMMAND)
             self.send(self.buffer_out)
-
 
     def get_file_listing(self):
         """
@@ -134,7 +131,7 @@ class Connection(object):
         self.buffer_out += listing
         self.send(self.buffer_out)
 
-    def get_metadata(self, filename : str):
+    def get_metadata(self, filename: str):
         """
         Devuelve el tamaño del archivo dado en bytes
         """
@@ -153,8 +150,7 @@ class Connection(object):
             self.buffer_out += f"{str(data)}"
             self.send(self.buffer_out)
 
-
-    def get_slice(self, filename : str, offset : str, size : str):
+    def get_slice(self, filename: str, offset: str, size: str):
         if not self.file_exist(filename):
             self.buffer_out = self.__mk_code(FILE_NOT_FOUND)
             self.send(self.buffer_out)
@@ -193,13 +189,12 @@ class Connection(object):
                     self.buffer_out = ''
                     self.send(self.buffer_out)
 
-    def _recv(self, timeout=None):
+    def _recv(self):
         """
         Recibe datos y acumula en el buffer interno.
 
         Para uso privado del server.
         """
-        self.socket.settimeout(timeout)
         try:
             data = self.socket.recv(4096).decode("ascii")
             self.buffer_in += data
@@ -213,8 +208,7 @@ class Connection(object):
             self.connection_active = False
             print("Closing connection...")
 
-
-    def read_line(self, timeout=None):
+    def read_line(self):
         """
         Espera datos hasta obtener una línea completa delimitada por el
         terminador del protocolo.
@@ -222,21 +216,15 @@ class Connection(object):
         Devuelve la línea, eliminando el terminaodr y los espacios en blanco
         al principio y al final.
         """
-        while not EOL in self.buffer_in and self.connection_active:
-            if timeout is not None:
-                t1 = time.process_time()
-            self._recv(timeout)
-            if timeout is not None:
-                t2 = time.process_time()
-                timeout -= t2 - t1
-                t1 = t2
+        while EOL not in self.buffer_in and self.connection_active:
+            self._recv()
+
         if EOL in self.buffer_in:
             request, self.buffer_in = self.buffer_in.split(EOL, 1)
             return request.strip()
         else:
             self.connected = False
             return ""
-
 
     def handle(self):
         """
@@ -249,7 +237,7 @@ class Connection(object):
                 self.send(self.buffer_out)
                 self.connection_active = False
                 print("Closing connection...")
-            elif len(response)>0:
+            elif len(response) > 0:
                 try:
                     self.analizar_comando(response)
                 except Exception:
